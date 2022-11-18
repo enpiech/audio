94f52b7c7b7f9da6f87bc75147390cd701555f36
Huy Phan <enpiech@gmail.com>
2022-11-18 11:06:17 +0700

    FEAT: - add PlaySFXAction

---
+++ Packages/Scripts/Runtime/Action/PlaySFXAction.cs
@@ -0,0 +1,28 @@
+﻿using Enpiech.Audio.Runtime.Data;
+using UnityAtoms;
+using UnityEngine;
+
+namespace Enpiech.Audio.Runtime.Action
+{
+    [EditorIcon("atom-icon-purple")]
+    [CreateAssetMenu(menuName = "Unity Atoms/Actions/Audio/Play SFX", fileName = "AC_PlaySFX")]
+    public sealed class PlaySFXAction : AtomAction
+    {
+        [Header("Config")]
+        [SerializeField]
+        private AudioCueSO _audioCue = default!;
+
+        [SerializeField]
+        private AudioConfigurationSO _config = default!;
+
+        [Header("Broadcasting on")]
+        [SerializeField]
+        private AudioCueEventChannelSO _playSfxAudioCueEventChannel = default!;
+
+        public override void Do()
+        {
+            base.Do();
+            _playSfxAudioCueEventChannel.RaisePlayEvent(_audioCue, _config);
+        }
+    }
+}
